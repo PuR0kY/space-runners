cc074307c08706a77e33d8d61bc7b4292dacfe54
Matěj Pur <mattpur@seznam.cz>
2025-08-17 22:40:28 +0200

    s

--- Scenes/Space/Space Generation/Planet/areas.gd
+++ Scenes/Space/Space Generation/Planet/areas.gd
@@ -3,9 +3,7 @@ extends Node3D
 signal spaceship_entered(resolution: int)
 
 @export var area_3d: Area3D
-@export var gravity_strength: float
-@export var gravity_range: float
-@export var rotation_speed_deg: float
+@onready var planet: Planet = $".."
 
 const CLOSE_RESOLUTION = 650
 const MID_RESOLUTION = 350
@@ -23,13 +21,13 @@ func _physics_process(delta: float) -> void:
 			# Gravitační síla
 			var dir: Vector3 = global_transform.origin - body.global_transform.origin
 			var dist: float = dir.length()
-			var force: Vector3 = dir.normalized() * gravity_strength * (1.0 - dist / gravity_range)
+			var force: Vector3 = dir.normalized() * planet.gravity_strength * (1.0 - dist / planet.gravity_range)
 			body.set_gravity_force(force * -1)
 
 			# Rotace kolem středu planety
 			var center = global_transform.origin
 			var rot_dir = body.global_transform.origin - center
-			var angle = deg_to_rad(rotation_speed_deg) * delta
+			var angle = deg_to_rad(planet.rotation_speed_deg) * delta
 			rot_dir = rot_dir.rotated(Vector3.UP, angle)
 			body.global_transform.origin = center + rot_dir
 
@@ -75,7 +73,3 @@ func _on_mid_area_body_exited(body: Node3D) -> void:
 	if body is not Spaceship:
 		return;
 	emit_signal("spaceship_entered", FAR_RESOLUTION)
-
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
